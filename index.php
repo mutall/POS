59d168fe4438401ba721a6b97d20cab3cbbf4fc5
amshel <amshelhack3r@gmail.com>
2019-12-10 14:36:35 +0000

    add sulphur fonts

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="animate.css" />
+    <link href="https://fonts.googleapis.com/css?family=Sulphur+Point&display=swap" rel="stylesheet">
     <title>Point Of Sale</title>
 
 </head>
@@ -22,6 +23,7 @@
             <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
             PRODUCT QUANTITY UPDATED
         </div>
+        <h3>INVENTORY MANAGEMENT</h3>
         <input class="animated pulse" type="text" name="" id="barcode" placeholder="BARCODE">
         <!-- Trigger/Open The Modal -->
         <button id="myBtn">ENTER NEW PRODUCT</button>
@@ -30,42 +32,36 @@
 
 
     <!-- The Modal -->
-    <div id="myModal" class="modal">
+    <div id="myModal" class="modal animated">
 
         <!-- Modal content -->
         <div class="modal-content">
             <span class="close">&times;</span>
-            <form action="insert.php" method="post">
+            <form id="form">
                 <div class="row">
-                    <label for="name">Enter product name</label>
-                    <input type="text" name="name" id="name" placeholder="Enter Product Name" required>
+                    <label for="name">Enter name:</label>
+                    <input type="text" name="name" id="name" required>
                 </div>
                 <div class="row">
-                    <label for="unique">Enter Barcode</label>
-                    <input type="text" name="unique" id="unique" placeholder="Enter Barcode" required>
+                    <label for="unique">Enter Barcode:</label>
+                    <input type="text" name="unique" id="unique" required>
                 </div>
                 <div class="row">
-                    <label for="quantity">Enter Quantity</label>
-                    <input type="text" name="quantity" id="quantity" placeholder="Enter Quantity">
+                    <label for="quantity">Enter Quantity:</label>
+                    <input type="text" name="quantity" id="quantity">
                 </div>
                 <div class="row">
-                    <label for="amount">Enter Amount</label>
-                    <input type="text" name="amount" id="amount" placeholder="Enter amount" required>
+                    <label for="amount">Enter Amount:</label>
+                    <input type="text" name="amount" id="amount" required>
                 </div>
                 <div class="buttons">
-                    <input type="submit" value="UPLOAD">
-                    <input type="reset" value="CLEAR">
+                    <input type="submit" id="upload-btn" value="UPLOAD">
+                    <input type="reset" id="reset-btn" value="CLEAR">
                 </div>
             </form>
         </div>
 
     </div>
-
-
-
-
-
-
     <script src="main.js"></script>
 </body>
 
