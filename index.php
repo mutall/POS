d862476762bb26b8a4644b59a6583bad451fbc34
amshel <amshelhack3r@gmail.com>
2020-01-14 14:12:35 +0000

    add sections for stock taking

--- index.php
+++ index.php
@@ -4,34 +4,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
     <title>POINT OF SALE</title>
-    <style>
-        body {
-            font-family: 'Sulphur Point', sans-serif;
-            font-size: 20px;
-            background-color: #F4511E;
-        }
-        .container{
-            width: 80%;
-            margin: 0 auto;
-        }
-    </style>
 </head>
 
 <body>
     <div class="container">
-        <h1>POINT OF SALE</h1>
-        <h3>DASHBOARD</h3>
+        <h1 class="text-center">POINT OF SALE</h1>
+        <h3 class="text-center">DASHBOARD</h3>
 
-        <div class="content">
-            <button class="stock">
-                STOCK TAKING
-            </button>
+        <div class="content mt-5">
+            <section id="s1" class="sect active">
+                <button class="stock big-button" target="stock1">
+                    STOCK
+                </button>
+            </section>
         </div>
+
+        <section id="stock1" class="sect">
+            <button class="big-button" onclick="showStock(3)">STORE STOCK</button>
+            <button class="big-button" onclick="showStock(1)">UPPER CHICJOINT COUNTER</button>
+            <button class="big-button" onclick="showStock(2)">LOWER CHICJOINT COUNTER</button>
+        </section>
+
+        <section id="counter-chooser" class="sect">
+            
+        </section>
     </div>
 </body>
-<script>
-    
-</script>
+<script src="./scripts/navigator.js"></script>
+
 </html>
