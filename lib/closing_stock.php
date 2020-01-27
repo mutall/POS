0801dc06de8e52197e92c3ca8d22327d0b7be15e
amshel <amshelhack3r@gmail.com>
2020-01-27 17:02:43 +0000

    fetched the product name and latest closing stock from server

---
+++ lib/closing_stock.php
@@ -0,0 +1,26 @@
+<?php
+// require the database file
+require_once "database.php";
+
+// creating a new instance of database.
+$database = new Database;
+
+// create an sql querry to calculate the maximum date
+$max_date = "SELECT product, max(stock.date) as max from stock group by product";
+
+// create an sql querry to get product
+$product_details = "SELECT 
+                        product.name, stock.date, stock.quantity
+                    FROM 
+                        product 
+                            INNER JOIN stock ON product.product = stock.product
+                            INNER JOIN ($max_date) as max_date ON product.product = max_date.product
+                    WHERE 
+                        stock.date = max_date.max";
+                                                            
+// Querry the sql
+$result = $database->query($product_details);
+
+//echo the results to javascript
+echo json_encode($result->fetchAll(PDO::FETCH_OBJ));
+                    
