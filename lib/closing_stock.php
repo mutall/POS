9ef77840d77024bbcbcaf32030cc08583291ca34
amshel <amshelhack3r@gmail.com>
2020-02-03 16:30:27 +0000

    few changs

--- lib/closing_stock.php
+++ lib/closing_stock.php
@@ -10,11 +10,12 @@ $max_date = "SELECT product, max(stock.date) as max from stock group by product"
 
 // create an sql querry to get product
 $product_details = "SELECT 
-                        product.name, stock.date, stock.quantity
+                       product.product, product.name, stock.date, stock.quantity, image.name as image
                     FROM 
                         product 
                             INNER JOIN stock ON product.product = stock.product
                             INNER JOIN ($max_date) as max_date ON product.product = max_date.product
+                            INNER JOIN image ON image.product = product.product
                     WHERE 
                         stock.date = max_date.max";
                                                             
@@ -23,4 +24,3 @@ $result = $database->query($product_details);
 
 //echo the results to javascript
 echo json_encode($result->fetchAll(PDO::FETCH_OBJ));
-                    
