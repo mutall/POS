6abff11be4552b6d8a17c7c23f30dc37b7b5c503
amshel <amshelhack3r@gmail.com>
2020-03-03 01:07:11 +0000

    Start using model classes

---
+++ lib/Chicjoint.php
@@ -0,0 +1,172 @@
+<?php
+require_once 'models/product.php';
+require_once 'models/staff.php';
+require_once 'models/station.php';
+
+//TODO write a query that retirves the last closing balance for a particular product
+
+
+//get post data from javascript
+$data = json_decode(file_get_contents('php://input'));
+
+
+//check for class and method variables
+if(isset($data->class) && isset($data->method)){
+    run($data->class, $data->method, $data->state);
+}
+
+//create a  function that takes in a class name and method and runs the class and method
+function run($class, $method, $static = false){
+    if(class_exists($class)){
+        //check if method exists
+        if($static){
+            return (method_exists($class, $method)) ? $class::$method() : die("Class $class doesnt contain method $method");
+        }else{
+            $obj = new $class;
+            return (method_exists($obj, $method))? $obj->$method() : die("Class $class doesnt contain method $method");
+        }
+        
+    }else{
+        die("No class by the name $class");
+    }
+}
+
+
+/**
+ * This will be the main php entry file 
+ * Most database operations will be handled by this file 
+ */
+
+class ChicJoint 
+{
+    /**
+     * This method will be called before the system initialises.
+        why?? 
+        The querys all pull values that are always constant in the system i.e product names, staff members stations
+     */
+    public function init()
+    {
+        $arr = [];
+        $product = new Product;
+        $staff = new Staff;
+        $station = new Station;
+
+        $arr['products'] = $product->records();
+        $arr['staff'] = $staff->records();
+        $arr['station'] = $station->records();
+
+        echo json_encode($arr);
+    }
+  
+    //get stok of a counter for  particular day
+    static public function getStock()
+    {
+        global $data;
+        $arr = array();
+        
+       
+
+        echo json_encode($arr);
+    }
+
+    /**
+     * function for getting the closing stock of a partricular product
+     * pass the station as a parameter
+     */
+    static public function getClosingStock()
+    {
+
+        $arr = [];
+        
+        echo json_encode($arr);
+    }
+
+}
+//this code is for inserting barcode readings 
+class BarcodeController
+{
+    public $db;
+    public $barcode;
+    public $data;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+        if (sizeof($_POST) > 0) {
+            if (isset($_POST['type'])) {
+                $this->update_quantity();
+            } else {
+                //insert new record
+                $this->insertNewBarcode();
+            }
+        } else {
+            //scan a barcode
+            $postData = file_get_contents('php://input');
+            $this->data = json_decode($postData);
+            $this->barcode = $this->data->barcode;
+            $this->checkBarcodeInDb();
+        }
+    }
+
+
+    function checkBarcodeInDb()
+    {
+        $sql = "SELECT barcode FROM product WHERE barcode = '$this->barcode' ";
+        $result = $this->db->query($sql);
+
+        if ($result->rowCount() == 0) {
+            http_response_code(404);
+            return;
+        } elseif ($result->rowCount() > 0) {
+            $this->incrementCount();
+            http_response_code(202);
+            return;
+        } else {
+            http_response_code(500);
+            die("Invalid output");
+        }
+    }
+
+    function incrementCount()
+    {
+        $sql = "SELECT quantity FROM product WHERE barcode = '$this->barcode'";
+        $result = $this->db->query($sql);
+        $quantity = $result->fetchObject()->quantity;
+        $quantity++;
+        $update = "UPDATE product SET quantity = $quantity WHERE barcode = '$this->barcode'";
+        $this->db->exec($update);
+    }
+    function update_quantity()
+    {
+        $barcode = $_POST['scanned'];
+        $sql = "select quantity from product where barcode = '$barcode'";
+        $result = $this->db->query($sql);
+        $quantity = $result->fetchobject()->quantity;
+        $quantity += $_POST['quantity'];
+        $update = "update product set quantity = $quantity where barcode = '$barcode'";
+        $this->db->exec($update);
+        http_response_code(202);
+    }
+
+    function insertNewBarcode()
+    {
+        $product_name = $_POST['name'];
+        $this->barcode = $_POST['unique'];
+        $quantity = 0;
+
+        if (isset($_POST['quantity'])) {
+            $quantity = intval($_POST['quantity']);
+        }
+        $amount = $_POST['amount'];
+
+
+        $sql = "INSERT INTO product
+                    (name, barcode, quantity, amount, client) 
+                VALUES
+                    ('$product_name', '$this->barcode', $quantity, '$amount', 1)";
+
+        $this->db->exec($sql);
+
+        http_response_code(201);
+    }
+}
