ec8112239e99221c4bf15fd8a3ce1780bf685d78
amshel <amshelhack3r@gmail.com>
2020-03-04 16:40:10 +0000

    create insert statement for session model

--- lib/models/session.php
+++ lib/models/session.php
@@ -1,11 +1,12 @@
 <?php
-require __DIR__."/../BaseModel.php";
+require __DIR__ . "/../BaseModel.php";
 require_once 'station.php';
 require_once 'staff.php';
 
 
-class Session extends BaseModel{
-    private DateTime $date;
+class Session extends BaseModel
+{
+    private String $date;
     private String $direction;
     private Station $station;
     private Staff $staff;
@@ -16,8 +17,26 @@ class Session extends BaseModel{
         parent::__construct();
     }
 
-    public function create($arg):Session
+    public function create($arg): Session
     {
+        $session = new Session();
+        $session->station = $arg->station;
+        $session->staff = $arg->staff;
+        $session->direction = $arg->direction;
+        $session->date = $arg->date;
+
+        $sql = "INSERT INTO " . $this->tableName .
+            "(staff, station, direction, date) 
+            VALUES
+            ('$this->staff->staff', '$this->station->station', '$this->direction', '$this.date')";
+        try {
+            $res = self::$db->exec($sql);
+            if($res!= false){
+                $session->id = self::$db->lastInsertId();
+            }
+        }catch (PDOException $e){
+            die($e->getMessage());
+        }
         return new Session();
     }
 }
